e1bbd6794b54d0c15453baeab52d894fc9f5e469
skn <svenyk@gmail.com>
2025-01-28 10:58:55 +0100

    sdfsdf

--- CLI-RENTING-SERVICE/Program.cs
+++ CLI-RENTING-SERVICE/Program.cs
@@ -7,9 +7,20 @@ while(true){
     input = Console.ReadLine();
     if(input == null){
         Environment.Exit(1);
-    }else if(input.ToLower() == "exit"){
-        Environment.Exit(0);
     }
 
-    Console.WriteLine(String.Format("Echo: {0}!", input));
+    switch(input){
+        case "0":
+            Environment.Exit(0);
+            break;
+        case "1":
+            break;
+        case "2":
+            break;
+        default:
+            Console.WriteLine("Invalid Selection!");
+            break;
+    }
+
+    Console.WriteLine(String.Format("Echo: {0}", input));
 }
