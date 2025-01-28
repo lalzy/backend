e7f886fbe6408f6941ccf62523e766a32ab287bf
skn <svenyk@gmail.com>
2025-01-28 10:54:21 +0100

    dsfsdf

--- CLI-RENTING-SERVICE/Program.cs
+++ CLI-RENTING-SERVICE/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Starting Renting Service");
+
+String? input;
+
+while(true){
+    input = Console.ReadLine();
+    if(input == null){
+        Environment.Exit(1);
+    }else if(input.ToLower() == "exit"){
+        Environment.Exit(0);
+    }
+
+    Console.WriteLine(String.Format("Echo: {0}!", input));
+}
