ae45f6e820244daadf364f4fd7a46bf4226c5adc
skn <svenyk@gmail.com>
2025-01-30 11:28:53 +0100

    khlhk

---
+++ Renting-Service/DataTransferObjects.cs
@@ -0,0 +1,4 @@
+class BorrowRequest {
+    public required string Message {get;set;}
+    public required int Number {get; set; }
+}
--- Renting-Service/Program.cs
+++ Renting-Service/Program.cs
@@ -1,7 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", () => "testing");
+app.MapGet("/", (String x = "") => {
+    //var jsonPayload = new {message = "Hello", content = "testing testing"};
+    //return Results.Ok(jsonPayload);
+});
+
+app.MapPost("/", (BorrowRequest requestBody) => {
+    Console.WriteLine($"{requestBody.Message}");
+    Console.WriteLine($"{requestBody.Number}");
+    return Results.Accepted("");
+});
+
+app.MapPut("/{articleId}", (int articleId) => {
+    Console.WriteLine(articleId);
+    return Results.Accepted();
+});
+
+app.MapDelete("/", () => {
+    return Results.Created();
+});
 
 app.Run();
+
