8aaecaa0d3512b2609afd6db67965de52bf1f99a
skn <svenyk@gmail.com>
2025-02-05 14:43:32 +0100

    Satt til å kunne hente data fra SQL - test.

--- todo-applikasjon/Program.cs
+++ todo-applikasjon/Program.cs
@@ -12,33 +12,52 @@ var app = builder.Build();
 // postgres=# GRANT ALL PRIVILEGES ON DATABASE mydatabase TO bruker
 // postgres-# 
 
+const string USER = "bruker";
+const string PASSWORD = "Zaq12345";
+const string DATABASE = "postgres";
 
-String setupDatabase(string user="bruker", string password="Zaq12345", string database="mydatabase"){
-    string connString = $"Host=localhost;Username={user};Password={password};Database={database}";
+String getConnString(string user="user", string password="", string database="mydatabase"){
+    return $"Host=localhost;Username={user};Password={password};Database={database}";
 
+}
 
+string connString = getConnString(USER, PASSWORD, DATABASE);
 
+List<String> getData(string connString){
     using (var conn = new NpgsqlConnection(connString))
     {
         conn.Open();
 
         // Query the database
-        using (var cmd = new NpgsqlCommand("SELECT version();", conn))
+        using (var cmd = new NpgsqlCommand($"SELECT * FROM oppgave;", conn))
         {
-            var version = cmd.ExecuteScalar().ToString();
-            return $"PostgreSQL version: {version}";
+            using (var reader = cmd.ExecuteReader()){
+                List<string> results = new List<string>();
+                while(reader.Read()){
+                     string row = $"{reader["titel"]}, {reader["beskrivelse"]}, {reader["status"]}";
+                    results.Add(row);
+                }
+                return results;
+            }
         }
     }
 }
 
-string test = setupDatabase();
+void setData(oppgaveRequest data){
+    
+}
 
 app.MapGet("/", () => {
+    string test = "";
+    foreach(string text in getData(connString)){
+        test += text;
+    }
     return Results.Ok(test);
 });
 
 
 app.MapPost("/add", ()=>{
+    Console.WriteLine("tet");
     return Results.Ok("post");
 });
 
